11362d95657adff12ac0cacfb92c73692dc25893
PanTruskawka045 <pan.truskawka045@gmail.com>
2026-08-24 17:40:09 +0200

    Initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("java")
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.18"
+    id("xyz.jpenilla.resource-factory-bukkit-convention") version "1.3.0"
+}
+
+group = "me.pan_truskawka045"
+version = "1.0-SNAPSHOT"
+
+var lombokVersion: String = "1.18.42"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.pantruskawka045.me/public")
+    }
+    mavenLocal()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("me.pan_truskawka045:Injector:1.0-SNAPSHOT")
+
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+    implementation("me.pan_truskawka045:Spigot-Animations:1.2.2-SNAPSHOT")
+
+    //This project will work on basic spigot, I'm using this only for QOL features.
+    paperweight.paperDevBundle(
+        version = "1.21.10-R0.1-SNAPSHOT",
+        group = "pl.kokscraft.strawberry-paper",
+        artifactId = "dev-bundle"
+    )
+
+}
+
+bukkitPluginYaml {
+    main = "me.pan_truskawka045.Slither.SpigotSlitherPlugin"
+    authors.add("PanTruskawka045")
+    apiVersion = "1.21.10"
+    version = project.version.toString()
+    name = "Spigot-Slither"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/me/pan_truskawka045/Slither/SpigotSlitherPlugin.java
@@ -0,0 +1,6 @@
+package me.pan_truskawka045.Slither;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotSlitherPlugin extends JavaPlugin {
+}
